8fc0acff82806fed3e64ac7bed58d1e0b2aa5a00
Conghuajidaner <49051565+Conghuajidaner@users.noreply.github.com>
2021-05-09 01:12:59 +0800

    Update 313.超级丑数.cpp

--- Leetcode/313.超级丑数.cpp
+++ Leetcode/313.超级丑数.cpp
@@ -1,5 +1,4 @@
-// ans是必须的
-
+// ans是必须的，笨一点的动态规划
 class Solution {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes) {
@@ -21,5 +20,32 @@ public:
             }
         }
         return ans[n - 1];
+    }
+};
+
+=============================
+//标程
+class Solution {
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+        if (n == 1) return 1;
+        int k = primes.size();
+        vector<int> dp(n, 1);
+        vector<int> p(k, 0);
+        vector<int> num(k, 0);
+        int count = 1;
+        for (int i = 0; i < k; i++) num[i] = primes[i] * dp[p[i]];
+        while (count < n) {
+            int m = *min_element(num.begin(), num.end());
+            dp[count] = m;
+            for (int i = 0; i < k; i++) {
+                if (num[i] == m) {
+                    p[i]++;
+                    num[i] = primes[i] * dp[p[i]];
+                }
+            }
+            count++;
+        }
+        return dp[n - 1];
     }
 };
