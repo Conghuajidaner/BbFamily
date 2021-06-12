dffb6ec355aadd405870c4a42d0e81d0f714b359
翟耀坤 <49051565+Conghuajidaner@users.noreply.github.com>
2021-06-12 16:48:41 +0800

    Create 3663.打印数字菱形.cpp

---
+++ Acwing/3663.打印数字菱形.cpp
@@ -0,0 +1,31 @@
+// this solution is accept
+// Train of thought: Set the number in the middle and extend to the left and right.
+#include <iostream>
+using namespace std;
+
+const int N = 30;
+char edge[N][N];
+
+int main()
+{
+    int n; cin >> n;
+    
+    for (int i = 0; i < 2*n + 1; ++ i)
+        for (int j = 0; j < 2*n + 1; ++ j)
+            edge[i][j] = ' ';
+
+    for (int i = n, j = n; j < 2*n + 1; i --, j ++)
+        edge[i][n] = edge[j][n] = '0' + i;
+    
+    for (int i = 0; i < 2*n + 1; ++ i)
+        for (int count = edge[i][n] - '0', j = n, k = n; count >= 0; --count, -- j, ++ k)
+            edge[i][j] = edge[i][k] = '0' + count;
+    
+    for (int i = 0; i < 2*n + 1; ++ i)
+    {
+        for (int j = 0; j < 2*n + 1; ++ j)
+            cout << edge[i][j] << ' ';
+        cout << endl;
+    }
+    return 0;
+}
