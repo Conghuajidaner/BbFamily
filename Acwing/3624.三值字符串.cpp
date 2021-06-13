0126e116c9d0280f057b0f5e2c12debabaa108ef
翟耀坤 <49051565+Conghuajidaner@users.noreply.github.com>
2021-06-14 00:06:46 +0800

    Update 3624.三值字符串.cpp

--- Acwing/3624.三值字符串.cpp
+++ Acwing/3624.三值字符串.cpp
@@ -1,5 +1,33 @@
+// this solution accept
+// 
 #include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int m; cin >> m;
+    while (m--)
+    {
+        string str; cin >> str;
+        int ans = INT_MAX;
+        int cnt[3] = {0};
+        for (int i = 0, j = 0; i < str.length(); ++ i)
+        {
+            cnt[str[i] - '1'] ++;
+            while (cnt[str[j] - '1'] > 1) cnt[str[j++] - '1']--;
+            if (cnt[0] && cnt[1] && cnt[2])
+                ans = min(ans, i - j + 1);
+        }
+        if (ans == INT_MAX) cout << 0 << endl;
+        else cout << ans << endl;
+    }
+    return 0;
+}
+
+
+// this solution is accept
+// Train of rhought:Map is less efficient
+#include<bits/stdc++.h>
 using namespace std;
 
 int main()
@@ -12,12 +40,12 @@ int main()
         map<char, int> cnt;
         for (; right < str.length(); )
         {
+            // This step doesn't have to satisfy this condition. if it does, update ans.
             while (right < str.length() && !(cnt['1'] > 0 && cnt['2'] > 0 && cnt['3'] > 0))
             {
                 cnt[str[right]] ++;
                 right++;
             }
-   
             while (left < right && cnt['1'] > 0 && cnt['2'] > 0 && cnt['3'] > 0)
             {
                 ans = min(right - left, ans);
