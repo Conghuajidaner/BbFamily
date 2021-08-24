68addaca77134ab1dac63eb8d14a3b8455accbe1
翟耀坤 <49051565+Conghuajidaner@users.noreply.github.com>
2021-08-24 15:58:58 +0800

    Create 3763. 数字矩阵.cpp

---
+++ Acwing/3763. 数字矩阵.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int numOfCase;
+    cin >> numOfCase;
+
+    while (numOfCase--)
+    {
+        int count = 0, sum = 0, minNum = 999999;
+        int n, m; cin >> n >> m;
+        auto buf = vector<vector<int>>(n, vector<int>(m));
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < m; ++j)
+            {
+                cin >> buf[i][j];
+                if (buf[i][j] <= 0) count ++;
+                sum += abs(buf[i][j]);
+                minNum = min(minNum, abs(buf[i][j]));
+            }
+        if (count % 2 == 1) {
+            cout << sum - 2*minNum << endl;
+        } else {
+            cout << sum << endl;
+        }
+    }
+
+    return 0;
+}
